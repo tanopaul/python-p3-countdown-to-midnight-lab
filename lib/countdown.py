8d18b37ad4781ac99584fbc3dae77f7d2f4c5342
Tano Paul <tanopaul38@gmail.com>
2023-09-12 17:00:12 -0400

    completed midnight lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,30 @@
 # your code goes here!
+import time
+
+def countdown(num):
+    counter = num
+
+    while counter > -1 :
+        if counter == 0:
+            print('HAPPY NEW YEAR!')
+        else:
+            print(f"{counter} SECOND(S)!")
+        counter-=1
+
+def countdown_with_sleep(num):
+    counter = num
+
+    while counter > -1 :
+        if counter == 0:
+            print('HAPPY NEW YEAR!')
+        else:
+            print(f"{counter} SECOND(S)!")
+        counter-=1
+        time.sleep(1)
+
+
+
+countdown_with_sleep(10)
+
+
+
